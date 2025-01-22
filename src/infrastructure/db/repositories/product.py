8480faa001bb56bf990d2b0d65a1950c0cb16928
Maxim <yrev2011@gmail.com>
2025-01-22 21:17:54 +0700

    Add product repository impl

---
+++ src/infrastructure/db/repositories/product.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.application.interfaces.repositories.product import IProductRepository
+from src.domain.models.product import Product
+from src.infrastructure.db.models.product import ProductModel
+
+
+class ProductRepository(IProductRepository):
+    domain = Product
+    model = ProductModel
+
+    def __init__(self, session: AsyncSession):
+        self._session = session
+
+    def _convert_domain_to_model(self, product: Product) -> ProductModel:
+        return self.model(
+            product_id=product.product_id,
+            name=product.name,
+            quantity=product.quantity,
+            price=product.price,
+        )
+
+    async def save(self, product: Product) -> None:
+        product_model = self._convert_domain_to_model(product)
+        self._session.add(product_model)
+        await self._session.commit()
