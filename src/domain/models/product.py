876483680d20fa51386668429d496e6d20d7c250
Maxim <yrev2011@gmail.com>
2025-01-24 23:51:57 +0700

    Add category value

--- src/domain/models/product.py
+++ src/domain/models/product.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass, field
 
+from src.domain.values.category import Category
+
 
 @dataclass
 class Product:
     product_id: int | None = field(default=None, kw_only=True)
     name: str
+    category: Category
     quantity: int
     price: int
---
+++ src/domain/values/category.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Category(Enum):
+    BOOKS = "Книги"
+    EDUCATION = "Образование"
+    HEALTH = "Здоровье"
+    TOYS = "Игрушки"
+    FOOD = "Еда"
