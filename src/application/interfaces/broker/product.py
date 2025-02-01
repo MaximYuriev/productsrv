b9d3a71051041068619a1a38be462a8c51d9bf2f
Maxim <yrev2011@gmail.com>
2025-02-01 16:03:22 +0700

    Edit constants naming

--- src/application/interfaces/broker/product.py
+++ src/application/interfaces/broker/product.py
@@ -4,10 +4,10 @@ from src.domain.models.product import Product
 
 
 class IProductPublisher(ABC):
-    _EXCHANGE = "product"
-    _CREATE_PRODUCT_QUEUE = "product-create"
-    _UPDATE_PRODUCT_QUEUE = "product-update"
-    _DELETE_PRODUCT_QUEUE = "product-delete"
+    _EXCHANGE_NAME = "product"
+    _CREATE_PRODUCT_QUEUE_NAME = "product-create"
+    _UPDATE_PRODUCT_QUEUE_NAME = "product-update"
+    _DELETE_PRODUCT_QUEUE_NAME = "product-delete"
 
     @abstractmethod
     async def publish_create_product(self, product: Product) -> None:
