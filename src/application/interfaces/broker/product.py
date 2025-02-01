a3e08d31f468be2a613ed25fbbae35326cf1bf68
Maxim <yrev2011@gmail.com>
2025-02-01 16:04:20 +0700

    Delete constants

--- src/application/interfaces/broker/product.py
+++ src/application/interfaces/broker/product.py
@@ -4,11 +4,6 @@ from src.domain.models.product import Product
 
 
 class IProductPublisher(ABC):
-    _EXCHANGE_NAME = "product"
-    _CREATE_PRODUCT_QUEUE_NAME = "product-create"
-    _UPDATE_PRODUCT_QUEUE_NAME = "product-update"
-    _DELETE_PRODUCT_QUEUE_NAME = "product-delete"
-
     @abstractmethod
     async def publish_create_product(self, product: Product) -> None:
         pass
